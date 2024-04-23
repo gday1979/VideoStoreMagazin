1a24f46b12a0d6c5ed44c28b5cdf04501eab3aea
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-04-23 17:39:06 +0300

    first commit

---
+++ WebVideoStore/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿namespace WebVideoStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
