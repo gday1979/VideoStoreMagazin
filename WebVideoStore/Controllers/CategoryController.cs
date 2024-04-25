753958ad8eebcf12e3c191863f6c97acffb38bc6
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-04-25 09:29:01 +0300

    create ApplicationDbContext

---
+++ WebVideoStore/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿namespace WebVideoStore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WebVideoStore.Data;
+
+    public class CategoryController : Controller
+    {
+        public CategoryController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebVideoStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace WebVideoStore.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using WebVideoStore.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+            public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                    new Category { Id = 2, Name = "Comedy", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "Drama", DisplayOrder = 3 },
+                    new Category { Id = 4, Name = "Horror", DisplayOrder = 4 },
+                    new Category { Id = 5, Name = "Sci-Fi", DisplayOrder = 5 }
+                );
+        }
+    
+    }
+    
+}
