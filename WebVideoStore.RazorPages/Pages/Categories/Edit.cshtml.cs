cf8f591bc72eec1df7b4a8c1b4ad22a34d28e957
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-04-27 16:46:38 +0300

    create Razor CrudOpeartions

--- WebVideoStore.RazorPages/Pages/Categories/Edit.cshtml.cs
+++ WebVideoStore.RazorPages/Pages/Categories/Edit.cshtml.cs
@@ -6,27 +6,37 @@ using WebVideoStore.RazorPages.Models;
 namespace WebVideoStore.RazorPages.Pages.Categories
 {
     [BindProperties]
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-        [BindProperty]
+       
         public Category Category { get; set; }
 
-        public CreateModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public void OnGet()
+        public void OnGet(int? id)
         {
+            if (id  != null && id != 0)
+            {
+            Category = _db.Categories.Find(id);
+               
+            }
         }
+
         public IActionResult OnPost()
         {
-            
-                _db.Categories.Add(Category);
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
                 _db.SaveChanges();
-                TempData["success"] = "Category created successfully";
+
+            TempData["success"] = "Category updated successfully";   
                 return RedirectToPage("Index");
-            
+            }
+            return Page();
+           
         }
     }
 }
