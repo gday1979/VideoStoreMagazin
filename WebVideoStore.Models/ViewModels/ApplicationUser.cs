57efdd6efbc5e4df1511b72e99b7ed78d7f856e2
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-05-18 14:45:32 +0300

    add CompanytoUser

--- WebVideoStore.Models/ViewModels/ApplicationUser.cs
+++ WebVideoStore.Models/ViewModels/ApplicationUser.cs
@@ -1,9 +1,11 @@
 ﻿namespace WebVideoStore.Models.ViewModels
 {
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -19,5 +21,10 @@
         public string? State { get; set; }
 
         public string? PostalCode { get; set; }
+
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
     }
 }
